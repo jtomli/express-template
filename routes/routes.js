3b30e26c123f3d99a93a2a55289eeebd61059977
spatel19 <smeetyboy19@icloud.com>
2017-07-15 02:41:25 -0700

    Update signup styling

--- routes/routes.js
+++ routes/routes.js
@@ -39,8 +39,66 @@ router.post('/wishlist', function(req, res) {
 let placeId;
 let venues = [];
 
+// router.post('/info', function(req, res) {
+//   console.log("search", req.session);
+//   if (req.session.search.length > 0) {
+//     console.log("search has items");
+//     res.render('list', {venues: req.session.search});
+//   } else {
+//     var options = {
+//       provider: 'google',
+//       httpAdapter: 'https', // Default
+//       apiKey: process.env.GOOGLEPLACES
+//     };
+//     var geocoder = NodeGeocoder(options);
+//     let lat;
+//     let long;
+//     geocoder.geocode(req.body.location)
+//     .then(function(response) {
+//       lat = response[0].latitude;
+//       long = response[0].longitude;
+//     })
+//     .then(function() {
+//       let radius = parseInt(req.body.radius) * 1609;
+//       let type = req.body.type.split(" ").join("_").toLowerCase();
+//       return request(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.GOOGLEPLACES}&location=${lat},${long}&radius=${radius}&type=${type}`)
+//       .then(resp => JSON.parse(resp))
+//       .then(obj => {
+//         placeId = [];
+//         obj.results.forEach(item => {
+//           placeId.push(item.place_id)
+//         });
+//
+//         for (var i=0; i<placeId.length; i++){
+//             venues.push(
+//                 request(`https://maps.googleapis.com/maps/api/place/details/json?key=${process.env.GOOGLEPLACES}&placeid=${placeId[i]}`)
+//                 .then(resp => JSON.parse(resp))
+//                 .then(obj2 =>({
+//                   name: obj2.result.name,
+//                   address: obj2.result.formatted_address,
+//                   phone: obj2.result.formatted_phone_number,
+//                   photos: obj2.result.photos,
+//                   rating: obj2.result.rating,
+//                   hours: obj2.result.opening_hours ? obj2.result.opening_hours.weekday_text : ["Not found"],
+//                   type: obj2.result.types,
+//                   url: obj2.result.url,
+//                   website: obj2.result.website,
+//                   link: 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='+ obj2.result.photos[0].photo_reference + '&key='+ process.env.GOOGLEPLACES
+//               })))
+//         }
+//         console.log('venues', venues[1]);
+//         return Promise.all(venues)
+//       }).then(arrayOfResults => {
+//         console.log("done!!!!!", arrayOfResults);
+//         req.session.search = arrayOfResults;
+//         res.render('list', {venues: arrayOfResults});
+//       })
+//       .catch(err => console.log("ERR", err))
+//   }
+// }
+// })
+
 router.post('/info', function(req, res) {
-  console.log("search", req.session);
   if (req.session.search.length > 0) {
     console.log("search has items");
     res.render('list', {venues: req.session.search});
@@ -92,10 +150,14 @@ router.post('/info', function(req, res) {
         console.log("done!!!!!", arrayOfResults);
         req.session.search = arrayOfResults;
         res.render('list', {venues: arrayOfResults});
-      })
-      .catch(err => console.log("ERR", err))
+      }).catch(err => console.log("ERR", err))
+    }).catch(function(err) {
+      console.log(err);
+    });
   }
 })
+
+
 
 // router.get('/:venueid', function(req, res) {
 //   var sampleVenue = {
